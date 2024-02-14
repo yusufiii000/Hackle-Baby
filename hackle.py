ec899b4c2ab4591b9decf0b206878e5675acebad
yusufiii000 <158528545+yusufiii000@users.noreply.github.com>
2024-02-14 23:28:39 +0300

    hackle.py

---
+++ hackle.py
@@ -0,0 +1,156 @@
+#Script By Yusufiii Hackk
+
+import os
+os.system('clear')
+
+print('TOOLUNUZ Başlıyor')
+print('')
+print('TOOLUNUZ KURULUYOR...')
+print('')
+
+import sys
+import time
+
+def loading_animation():
+    chars = "<>\|/##@@"
+    for i in range(20):
+        sys.stdout.write("\r" + chars[i % len(chars)] + " Kuruluyor...")
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+loading_animation()
+print('')
+print('')
+
+a=int(input('+[1]- e Basıp Enter Basınız == '))
+print('')
+
+import sys
+import time
+
+def loading_animation():
+    chars = "<>\|/##@@"
+    for i in range(20):
+        sys.stdout.write("\r" + chars[i % len(chars)] + " YÜKLENİYOR...")
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+loading_animation()
+
+import os 
+os.system('clear')
+
+print('')
+b=int(input('+[2]- Bas Enter Bas == '))
+
+b==2
+print('')
+
+
+import os
+os.system('clear')
+
+print('TOOL BAŞLIYOR')
+print('')
+
+import sys
+import time
+
+def loading_animation():
+    chars = "<>\|/##@@"
+    for i in range(20):
+        sys.stdout.write("\r" + chars[i % len(chars)] + " Başlıyor...")
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+loading_animation()
+print('')
+print('')
+print('Merhabalar Nasılnız?')
+print('')
+b=int(input("Sohbet İçin +[1]- Bas Enter Bas == "))
+c=int(input("TOOL Devam Etmek İçin +[2]- Bas Enter Bas == "))
+print('')
+if b==1 :print('Sohbet İçin İnstagram Veya İş Birliği İçin Dm İnstagram languishing_yusuf')
+c==2 
+print('')
+print('TOOL Devam Ediliyor...')
+print('')
+d=int(input('+[31]- Bas Enter Bas Devam Et... == '))
+d==31
+print('')
+import os
+os.system('clear')
+
+import sys
+import time
+
+def loading_animation():
+    chars = "<>\|/##@@"
+    for i in range(20):
+        sys.stdout.write("\r" + chars[i % len(chars)] + " HAHAHAHA...")
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+loading_animation()
+
+print('')
+print('')
+print('TOOLS İS HACK STARTİNG')
+print('')
+import sys
+import time
+
+def loading_animation():
+    chars = "<>\|/##@@"
+    for i in range(20):
+        sys.stdout.write("\r" + chars[i % len(chars)] + " STARTİNG...")
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+loading_animation()
+print('')
+print('')
+
+import os 
+os.system('clear')
+
+
+
+import sys
+import time
+
+def loading_animation():
+    chars = "@@-/\|<>"
+    for i in range(20):
+        sys.stdout.write("\r" + chars[i % len(chars)] + " Lütfen Bekleyin...")
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+loading_animation()
+
+print("")
+print("")
+print('== Sonuç Vuruldu ¤')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
